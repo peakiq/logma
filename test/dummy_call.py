be6f446cb46d56fa64d621fbe1e4e88d84c233c7
Oliver Berger <oliver@digitalarchitekt.de>
2019-12-18 11:11:24 +0100

    merge whole default log config
    Incompatible chnage: A user might want to create new handlers or
    formatters, so he should be enabled.
    
    Removed unittest in favor of pytest.
    Renamed test to tests

--- test/dummy_call.py
+++
@@ -1,9 +0,0 @@
-import structlog
-
-logger = structlog.get_logger()
-
-
-def call():
-    logger.info("This info call")
-    logger.debug("This hidden debug call")
-    logger.info("This is another info call")

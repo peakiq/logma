58a788f1118a6aeef677a6385ea32c158ea1d757
Josip Delic <delijati@googlemail.com>
2020-03-04 12:32:16 +0100

    Back to development: 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="logma",
-    version="0.2.0",
+    version="0.2.1.dev0",
     author="peakiq",
     description="defaults for structlog",
     long_description=long_description,

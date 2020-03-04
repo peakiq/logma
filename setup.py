b8ca1d949f9ad4e7049f9ed79e088eca8746a625
Josip Delic <delijati@googlemail.com>
2020-03-04 12:32:04 +0100

    Preparing release 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="logma",
-    version="0.1.2.dev0",
+    version="0.2.0",
     author="peakiq",
     description="defaults for structlog",
     long_description=long_description,
